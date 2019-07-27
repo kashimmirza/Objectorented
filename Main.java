092fb4c27f4affdc1f1e673ac6b8c33ed76518e8
kashimmirza <45008789+kashimmirza@users.noreply.github.com>
2019-07-26 21:48:41 -0700

    Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+	public static void main(String []arg) {
+		
+		Student t=new Student ("kash",21,"ban",19812,"abc college");
+		t.Info();
+		t.printshow();
+		t.info();
+	}
+
+}
