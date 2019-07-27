cd2a61d35b008d22799663229f0c4dbc4376dd59
kashimmirza <45008789+kashimmirza@users.noreply.github.com>
2019-07-26 21:48:01 -0700

    Inhe0.java

---
+++ Inhe02.java
@@ -0,0 +1,19 @@
+
+public class Inhe02 {
+	 
+	String name;
+	int age;
+	String country;
+	 public Inhe02(String name, int age , String country){
+		this.name= name;
+		this.age=age;
+		this.country=country;
+		
+	}
+	 public void Info() {
+		 System.out.println(this.name+this.age+this.country);
+		 
+	 }
+	
+
+}
