e6a28430c2bbb874b72c1c3aabf6e066e57cf340
kashimmirza <45008789+kashimmirza@users.noreply.github.com>
2019-07-29 12:58:57 -0700

    Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,31 @@
 
-public class Main {
+public class Main0 {
 	public static void main(String []arg) {
 		
-		Student t=new Student ("kash",21,"ban",19812,"abc college");
-		t.Info();
-		t.printshow();
-		t.info();
+		/*Pets c1=new Cat("tumi",35.5,30);
+		Pets c2=new Dogs("komy",67.5,90);
+		System.out.println(c1.toString());
+		System.out.println(c1.howtosound());
+		System.out.println(c2.toString());
+		System.out.println(c2.howtosound());*/
+		Pets[]p=new Pets[3];
+		p[0]=new Cat("komi",8.5,24);
+		p[1]=new Dogs("nomi",89.8,30);
+		p[2]=new Cat("toto",90.0,90);
+		for(Pets k:p) {
+			if(k instanceof Cat) {
+				System.out.println(k.toString());
+				System.out.println(k.howtosound());
+			}
+			else {
+				System.out.println(k.howtosound());
+			}
+			
+			
+			
+		}
+		
+		
 	}
 
 }
